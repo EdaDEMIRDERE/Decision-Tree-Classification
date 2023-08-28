4285e39e1ba7500b6902a25d6f23badf1b6c3f15
Eda DEMIRDERE <demirdereda@hotmail.com>
2023-08-28 22:01:59 +0300

    Tahminleme yapılması

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ Y = [0, 1]
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, Y)
+
+print(clf.predict([[0.499, 0.499]]))
+# burada her bir sınıfa ait olasalık döndürür
+print(clf.predict_proba([[2, 2]]))
