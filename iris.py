e16b69cfed4b3f62ef9e4bea2516ad8c81316574
Eda DEMIRDERE <demirdereda@hotmail.com>
2023-08-28 22:06:21 +0300

    iris.pdf nin oluşturulması

--- iris.py
+++ iris.py
@@ -12,3 +12,14 @@ clf = clf.fit(X, y)
 plt.figure(figsize=(20, 10))
 tree.plot_tree(clf, filled=True, feature_names=iris.feature_names, class_names=[str(name) for name in iris.target_names])
 plt.show()
+
+# graphviz formatında temsil eden bir metin çıktısı üretir
+dot_data = tree.export_graphviz(clf, out_file=None,
+                                feature_names=iris.feature_names,
+                                class_names=iris.target_names,
+                                filled=True,
+                                rounded=True)
+# graphviz tarafından oluşturulan grafikleri temsil eder
+graph = graphviz.Source(dot_data)
+# oluşturulan grafik görselini "iris" adlı bir dosyaya kaydediyor
+graph.render("iris")
